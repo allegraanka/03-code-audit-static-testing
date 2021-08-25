20d9a7c5fabffabb78af01e43a623dea0bb467bb
Adam Chipperfield <adam.chipperfield@wemakewebsites.info>
2021-08-25 11:27:55 +0100

    BUILD: Added social icons and their stories

---
+++ app/stories/icons/social-twitter.stories.js
@@ -0,0 +1,16 @@
+/**
+ * @file Defines the story for the Twitter icon.
+ */
+import IconTwitter from '@/assets/icons/social-twitter.svg?inline'
+
+export default {
+  title: 'Icons/Social/Twitter'
+}
+
+export const Base = () => ({
+  components: {
+    IconTwitter
+  },
+
+  template: '<icon-twitter />'
+})
---
+++ app/stories/icons/social-youtube.stories.js
@@ -0,0 +1,16 @@
+/**
+ * @file Defines the story for the YouTube icon.
+ */
+import IconYouTube from '@/assets/icons/social-youtube.svg?inline'
+
+export default {
+  title: 'Icons/Social/YouTube'
+}
+
+export const Base = () => ({
+  components: {
+    IconYouTube
+  },
+
+  template: '<icon-you-tube />'
+})
