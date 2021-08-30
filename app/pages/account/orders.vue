ec718ef5b82f783c40718514fe69ef0e83d653b4
Adam Chipperfield <adam.chipperfield@wemakewebsites.info>
2021-08-30 10:33:57 +0100

    BUILD: Created the order listing page and query

--- app/pages/account/orders.vue
+++ app/pages/account/orders.vue
@@ -3,11 +3,31 @@
 </template>
 
 <script>
+import customerOrders from '@/graphql/shopify/queries/customerOrders'
+
 import Account from '~/components/Account'
 
 export default {
   components: {
     Account
+  },
+
+  async asyncData({ app, store }) {
+    const accessToken = store.state.customer.accessToken
+
+    if (!accessToken) {
+      store.dispatch('customer/logout')
+      return
+    }
+
+    const customer = await app.$graphql.shopify.request(customerOrders, {
+      customerAccessToken: accessToken
+    })
+
+    return {
+      orders: customer.customer.orders.edges.map(({ node }) => node),
+      error: customer ? false : true
+    }
   }
 }
 </script>
