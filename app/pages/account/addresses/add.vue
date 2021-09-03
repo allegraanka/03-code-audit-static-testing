7a6cf575eaceb4a6aa165aa7fd907ba192db1b2d
Adam Chipperfield <adam.chipperfield@wemakewebsites.info>
2021-09-03 18:17:38 +0100

    BUILD: Added the back button, title and submit button to the address add template

--- app/pages/account/addresses/add.vue
+++ app/pages/account/addresses/add.vue
@@ -1,13 +1,54 @@
 <template>
-  <account />
+  <account>
+    <nuxt-link class="template-addresses-add__back" to="/account/addresses">
+      <icon-arrow-back /> Back to Addresses
+    </nuxt-link>
+
+    <h2 class="template-addresses-add__title">Add Address</h2>
+
+    <div class="template-addresses-add__form">
+      <app-button @click.native="handleAddressAdd">Add new address</app-button>
+    </div>
+  </account>
 </template>
 
 <script>
 import Account from '~/components/Account'
+import AppButton from '~/components/AppButton'
+
+import IconArrowBack from '@/assets/icons/directional-arrow-backward.svg?inline'
 
 export default {
   components: {
-    Account
+    Account,
+    AppButton,
+    IconArrowBack
+  },
+
+  methods: {
+    /**
+     * Handles the address add event.
+     */
+    handleAddressAdd() {
+      // @todo handle address add mutation
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.template-addresses-add {
+  &__back {
+    align-items: center;
+    display: flex;
+
+    .icon {
+      margin-right: 0.625rem;
+    }
+  }
+
+  &__title {
+    margin-top: $SPACING_4XL;
+  }
+}
+</style>
