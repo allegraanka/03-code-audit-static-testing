57d245f3bc444f73d6c3a0d51ce46e0e27d8c83a
Adam Chipperfield <adam.chipperfield@wemakewebsites.info>
2021-09-13 17:30:00 +0100

    BUILD: Added color and text alignment options to the hero banner and image with text models

---
+++ sanity/scripts/styles.js
@@ -0,0 +1,45 @@
+/**
+ * @file Holds style data which can be surfaced as a setting option.
+ */
+
+export const colors = {
+  background: [
+    {
+      title: 'Dark',
+      value: 'dark'
+    },
+    {
+      title: 'Light',
+      value: 'light'
+    },
+    {
+      title: 'White',
+      value: 'white'
+    }
+  ],
+  themes: {
+    lightDark: [
+      {
+        title: 'Light',
+        value: 'light'
+      },
+      {
+        title: 'Dark',
+        value: 'dark'
+      }
+    ]
+  }
+}
+
+export const text = {
+  alignment: [
+    {
+      title: 'Left',
+      value: 'left'
+    },
+    {
+      title: 'Center',
+      value: 'center'
+    }
+  ]
+}
