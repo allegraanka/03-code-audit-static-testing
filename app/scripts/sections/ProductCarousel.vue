e83b20f96fd7757635423efd664b29b5922beb5e
Adam Chipperfield <adam.chipperfield@wemakewebsites.info>
2021-09-15 17:26:40 +0100

    FIX: Downgraded the Nacelle PIM linker and mocked a component to test the response

---
+++ app/scripts/sections/ProductCarousel.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="product-carousel"></section>
+</template>
+
+<script>
+export default {
+  props: {
+    products: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
