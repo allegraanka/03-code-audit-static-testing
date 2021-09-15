d12199eef075fb5c30b0e7fe6de8b48f49cf7888
Adam Chipperfield <adam.chipperfield@wemakewebsites.info>
2021-09-15 17:38:15 +0100

    BUILD: Updated the Product Carousel to fetch product data

--- app/scripts/sections/ProductCarousel.vue
+++ app/scripts/sections/ProductCarousel.vue
@@ -1,14 +1,66 @@
 <template>
-  <section class="product-carousel"></section>
+  <section class="product-carousel">
+    <div class="container">
+      <div class="row">
+        <div class="col xs12">
+          <div class="product-carousel__carousel">
+            <product-card
+              v-for="(product, index) in items"
+              :key="index"
+              :product="product"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProductCard from '~/components/ProductCard'
+
 export default {
+  components: {
+    ProductCard
+  },
+
   props: {
     products: {
       type: Array,
       default: () => []
     }
+  },
+
+  data() {
+    return {
+      items: []
+    }
+  },
+
+  fetch() {
+    this.fetchProducts()
+  },
+
+  methods: {
+    /**
+     * Requests and sets the product data.
+     */
+    fetchProducts() {
+      this.$nacelle.client.data
+        .products({ handles: this.products })
+        .then((response) => (this.items = response))
+        .catch((error) => void error)
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.product-carousel {
+  &__carousel {
+    display: grid;
+    gap: $SPACING_5XL;
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+</style>
